397727876de2f0c6027ed74521a67bc504ac777a
r3X <rch246chauhan@gmail.com>
2022-07-14 21:03:23 +0530

    Added Validation

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
-from market import app
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for
+
+from market import app, db
 from market.forms import RegisterForm
+from market.models import Item, User
 
 
 @app.route('/')
@@ -14,8 +15,18 @@ def market_page():
     items = Item.query.all()
     return render_template("market.html", items=items)
 
-@app.route("/register")
-def register_page():
-    form=RegisterForm()
-    return render_template("register.html", form=form)
 
+@app.route("/register", methods=["GET","POST"])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data, email=form.email.data, password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors!={}:
+        for err in form.errors.values():
+            print(f'There was an error with creating user: {err[0]}')
+
+
+    return render_template("register.html", form=form)
